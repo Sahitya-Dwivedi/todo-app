fd5997ad26c7d9fd4bd242df088884bc84bd4659
Sahitya-Dwivedi <sahityadwivedi515@gmail.com>
2024-08-24 22:42:46 +0530

    fixed iscompleted issue

--- app/page.js
+++ app/page.js
@@ -1,13 +1,11 @@
 "use client"
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { v4 as uuidv4 } from 'uuid';
 const home = () => {
   const [todo, settodo] = useState("")
   const [todos, setTodos] = useState([])
   const [iteration, setIteration] = useState(1)
-  const InputRef = useRef()
-  const todoTask = useRef()
-  
+
   useEffect(() => {
     const todoStr = localStorage.getItem("todos")
     if (todoStr) {
@@ -27,8 +25,7 @@ const home = () => {
   }
 
   let addTodo = () => {
-    setTodos([...todos, todo])
-    InputRef.current.value = ""
+    setTodos([...todos, { "todo": todo, "isCompleted": false }])
     settodo("")
   }
 
@@ -42,17 +39,16 @@ const home = () => {
   }
 
   let deleteTodo = (e) => {
-    const Updated_todoslist = todos.filter((i) => i != e.target.value)
+    const Updated_todoslist = todos.filter((i) => i.todo != e.target.value)
     setTodos(Updated_todoslist)
 
   }
 
-  let Check = (e) => {
-    if (e.target.checked) {
-      todoTask.current.style.textDecoration = "line-through"
-    } else {
-      todoTask.current.style.textDecoration = "none"
-    }
+  let isCheck = (e) => {
+    let index = todos.findIndex(val => val.todo == e.target.value)
+    let newTodos = [...todos]
+    newTodos[index].isCompleted = !newTodos[index].isCompleted
+    setTodos(newTodos)
   }
 
   return (
@@ -60,7 +56,7 @@ const home = () => {
       <div className="main w-1/2 min-w-60 h-[90vh]  bg-[#eff1f2] mx-auto mt-12 rounded-2xl border-2 border-blue-400 text-black">
         <h1 className="text-4xl font-bold italic font-sans mx-5 my-2 ">Your Todos</h1>
         <div className="input flex gap-2">
-          <input type="text" name="your todos" className="w-1/3 border-2 border-black ml-4 px-2 text-xl rounded-xl " placeholder="Enter Your Todo." value={todo} onChange={InputChange} ref={InputRef} />
+          <input type="text" name="your todos" className="w-1/3 border-2 border-black ml-4 px-2 text-xl rounded-xl " placeholder="Enter Your Todo." value={todo} onChange={InputChange} />
           <button className="add text-xl bg-blue-500 px-2 rounded-xl border-black border-2" onClick={addTodo}>Add</button>
         </div>
         <div className="todos w-[95%] h-[80%] bg-white border-2 border-[#000000a3] mx-auto mt-2 rounded-xl flex items-start gap-2 flex-col">
@@ -69,12 +65,12 @@ const home = () => {
               return (
                 <div key={uuidv4()} className='flex items-center gap-8'>
                   <div className='flex'>
-                    <input type="checkbox" name="checkbox" onChange={Check} className="mt-4 mx-4" />
-                    <div className="todo w-[95%] px-2 py-1  mt-4 text-center" ref={todoTask}>{todo}</div>
+                    <input type="checkbox" name="checkbox" onChange={isCheck} className="mt-4 mx-4" value={todo.todo} checked={todo.isCompleted}/>
+                    <div className={todo.isCompleted ? "todo w-[95%] px-2 py-1  mt-4 text-center line-through" : "todo w-[95%] px-2 py-1  mt-4 text-center"}>{todo.todo}</div>
                   </div>
                   <div className='flex gap-2'>
-                    <button className="edit mt-4 bg-blue-500 px-2 py-1 rounded-xl" onClick={editTodo} value={todo}>Edit</button>
-                    <button className="delete mt-4 bg-blue-500 px-2 py-1 rounded-xl" onClick={deleteTodo} value={todo}>Delete</button>
+                    <button className="edit mt-4 bg-blue-500 px-2 py-1 rounded-xl" onClick={editTodo} value={todo.todo}>Edit</button>
+                    <button className="delete mt-4 bg-blue-500 px-2 py-1 rounded-xl" onClick={deleteTodo} value={todo.todo}>Delete</button>
                   </div>
                 </div>
               )
