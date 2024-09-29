70440e23375e69125cc004f4eaf3cfeece170891
Sahitya-Dwivedi <sahityadwivedi515@gmail.com>
2024-09-29 16:47:16 +0530

    responsiveness

--- app/page.js
+++ app/page.js
@@ -2,6 +2,9 @@
 import React, { useEffect, useRef, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { FaCalendarDays } from "react-icons/fa6";
+import { FaEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+
 const Home = () => {
   const [todo, settodo] = useState("");
   const [todos, setTodos] = useState([]);
@@ -22,7 +25,7 @@ const Home = () => {
   useEffect(() => {
     if (iteration == 0) saveToLC();
     setIteration(0);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [todos]);
 
   const saveToLC = () => {
@@ -41,14 +44,14 @@ const Home = () => {
   };
 
   let editTodo = (e) => {
-    const value = JSON.parse(e.target.value)
+    const value = JSON.parse(e.target.value);
     settodo(value.todo);
-    setDescription(value.description)
-    setDueDate(value.DueDate)
+    setDescription(value.description);
+    setDueDate(value.DueDate);
     add.current.textContent = "Save";
     InpRef.current.focus();
     const Updated_todoslist = todos.filter((i) => i.todo != value.todo);
-    setTodos(Updated_todoslist)
+    setTodos(Updated_todoslist);
   };
 
   let deleteTodo = (e) => {
@@ -88,32 +91,37 @@ const Home = () => {
                 <div
                   className={
                     todo.isCompleted
-                      ? "todo w-[100%] px-2   mt-4 line-through font-bold italic"
-                      : "todo w-[95%] px-2   mt-4  font-bold italic "
+                      ? "todo w-fit px-2 mt-4 line-through italic text-nowrap"
+                      : "todo w-fit px-2 mt-4 italic text-nowrap"
                   }
                 >
                   {todo.todo}
                 </div>
-                <ul className="flex gap-2 items-center justify-center">
-                  <li className="text-gray-500 text-sm pl-2">
-                    {todo.description}
-                  </li>
-                  <li className="text-gray-500 text-sm list-disc list-inside">
-                    {todo.DueDate}
-                  </li>
-                </ul>
+                <div>
+                  <ul className="flex gap-2 items-center justify-center">
+                    <li className="text-gray-500 text-sm pl-2 text-balance">
+                      {todo.description}
+                    </li>
+                    <li
+                      className="text-gray-500 text-sm list-disc list-inside text-balance
+                    "
+                    >
+                      {todo.DueDate}
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
-            <div className="flex gap-2">
+            <div className="EditDelbtn flex gap-2">
               <button
-                className="edit mt-4 bg-blue-500 px-2 py-1 rounded-xl"
-                onClick={editTodo}
+                className="edit mt-4 bg-blue-500  px-2 py-1 rounded-xl relative z-10"
+                onClick={(e) => editTodo(e)}
                 value={JSON.stringify(todo)}
               >
                 Edit
               </button>
               <button
-                className="delete mt-4 bg-blue-500 px-2 py-1 rounded-xl mr-2"
+                className="delete mt-4 bg-blue-500 px-2  py-1 rounded-xl mr-2 box-border"
                 onClick={deleteTodo}
                 value={todo.todo}
               >
@@ -131,21 +139,28 @@ const Home = () => {
   };
 
   let dueDate = (e) => {
-    setDueDate(e.target.value);
+    let date = e.target.value;
+    let formattedDate = [];
+    let splitedDate = date.split("-");
+    formattedDate[0] = splitedDate[2];
+    formattedDate[1] = splitedDate[1];
+    formattedDate[2] = splitedDate[0];
+    let finalDate = formattedDate.join("-");
+    setDueDate(finalDate);
   };
 
   return (
     <>
-      <div className="main w-1/2 min-w-60 h-[90vh]  bg-[#eff1f2] mx-auto mt-12 rounded-2xl border-2 border-blue-400 text-black">
-        <h1 className="text-4xl font-bold italic font-sans mx-5 my-2 ">
+      <div className="main sm:w-1/2 min-w-[240px] h-screen w-screen sm:h-[90vh]  bg-[#eff1f2] mx-auto sm:mt-12 sm:rounded-2xl border-2 border-blue-400 text-black">
+        <h1 className="sm:text-4xl md:text-3xl font-bold italic font-sans mx-5 my-2 text-2xl">
           Your Todos
         </h1>
 
-        <div className="input flex gap-2">
+        <div className="input flex gap-2 ">
           <input
             type="text"
             name="your todos"
-            className="w-1/3 border-2 border-black ml-4 px-2 text-xl rounded-md "
+            className="w-1/2 md:w-1/3 border-2 border-black ml-4 px-1 sm:text-xl rounded-md text-sm "
             placeholder="Enter Your Todo."
             value={todo}
             onChange={(e) => settodo(e.target.value)}
@@ -169,7 +184,7 @@ const Home = () => {
           </div>
 
           <button
-            className="add text-xl bg-blue-500 px-2 rounded-xl border-black border-2"
+            className="add sm:text-xl bg-blue-500 px-2 rounded-xl border-black border-2"
             onClick={addTodo}
             ref={add}
           >
@@ -181,7 +196,7 @@ const Home = () => {
           type="text"
           name="description"
           id="description"
-          className="w-1/2 border-2 border-gray-500 mx-4 my-2 px-2 text-xl rounded-md"
+          className="w-10/12 sm:w-1/2 border-2 border-gray-500 mx-4 my-2 px-1 text-sm sm:text-xl rounded-md"
           placeholder="Enter description(optional)"
           onChange={(e) => setDescription(e.target.value)}
           value={description}
@@ -197,7 +212,7 @@ const Home = () => {
             onChange={handleFinish}
           />
         </div>
-        <div className="todos w-[95%] h-[65%] bg-white border-2 border-[#000000a3] mx-auto mt-2 rounded-md flex items-start gap-2 flex-col overflow-y-auto">
+        <div className="todos w-[95%] h-[65%] bg-white border-2 border-[#000000a3] mx-auto my-2 rounded-md flex items-start gap-2 flex-col overflow-y-auto sm:text-xl text-sm">
           {renderTask}
         </div>
       </div>
